adcf7f872a145eff226ffa96c31a8d2b2fd3cce0
Mateusz Radomski <Mateusz Radomski>
2020-03-12 21:38:48 +0100

    memory alloc

--- calc.s
+++ calc.s
@@ -14,6 +14,10 @@ txt_B:
 txt_C:
     .ascii "Podaj dzialanie:\n"
 
+a_var: .long 0x0
+b_var: .long 0x0
+int_size: .byte 8
+
 .text
 .global _start
 
@@ -40,10 +44,31 @@ txt_C:
    int $kernel
 .endm
 
+.macro alloc_memory size
+	mov %eax, $12 # get the current address of the program with brk
+	xor %ebx, %ebx
+	syscall
+
+	add %eax, \size # add the amount of memory we want to allocate
+	mov %ebx, %eax # as an arg
+	mov %eax, $12
+	syscall
+
+	cmp %eax, $0
+	jl _exit # we don't work if there is no memory
+	mov	%edi, %eax	 #EDI = highest available address
+	sub	%edi, $int_size		 #pointing to the last int  
+	mov	%ecx, \size	 #number of ints allocated
+	div %ecx, $int_size
+	mov %ecx, $eax
+	xor	%eax, %eax	 #clear eax
+	std			 #set the direction flag to backwards
+	rep	stosd            #do a memset to zero
+	cld			 #resert direction flag to normal
+.endm
 
 _start:
-
-    disp_str txt_A, $11
+	alloc_memory $16384 # alloc 16k of mem
 
 _exit:
     exit_prog $0
