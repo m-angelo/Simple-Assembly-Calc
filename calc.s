d179e8c3c4974b7fbf18a16928cea378d5c1e4a1
Mateusz Radomski <Mateusz Radomski>
2020-03-26 19:37:31 +0100

    Polling before reading in so as to not block

--- calc.s
+++ calc.s
@@ -1,6 +1,8 @@
 .equ read,0x03
 .equ write,0x04
 .equ exit,0x01
+.equ poll,168 # poll
+
 .equ stdin, 0x00
 .equ stdout, 0x01
 .equ kernel, 0x80
@@ -16,6 +18,12 @@ txt_C:
     .ascii "Podaj dzialanie:\n"
 io_buffer:
 	.ascii "                                                                "
+poll_fd:
+	.long 0x00
+poll_events:
+	.short 0x01 # POLLIN
+poll_revents:
+	.short 0x00
 
 .text
 .global _start
@@ -35,17 +43,37 @@ io_buffer:
 .endm
 
 _start:
-	mov $0x10, %eax # read 16 bytes
-	call read_stdin
+	mov $stdin, %eax # poll stdin
+	call poll_fd_read
 
 	cmp $0, %eax
 	jz _exit
 
+	mov $0x10, %eax # read 16 bytes
+	call read_stdin
+
     disp_str $txt_A, $11
 	disp_str $io_buffer, $64
-
 _exit:
     exit_prog $0
+
+#----------------------------------------
+#
+#	@func: poll_fd_read
+# 	@args: %eax - fd to poll
+# 	@retu: %eax - 0 if no data, anything else if theres data
+# 	@note: Does not save the eax!
+#
+.type poll_fd_read, @function
+poll_fd_read:
+	mov %eax, poll_fd
+	mov $poll, %eax
+	mov $poll_fd, %ebx
+	mov $0x01, %ecx
+	mov $0x00, %edx
+	int $kernel
+
+	ret
 
 #----------------------------------------
 #
