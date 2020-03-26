7289bb69f46e70ebc6a458cb9882c15b1d9d4131
Mateusz Radomski <Mateusz Radomski>
2020-03-26 18:45:29 +0100

    Reading from stdin

--- calc.s
+++ calc.s
@@ -1,7 +1,8 @@
+.equ read,0x03
 .equ write,0x04
-.equ exit,0x3c
+.equ exit,0x01
+.equ stdin, 0x00
 .equ stdout, 0x01
-.equ stdin, 0x03 
 .equ kernel, 0x80
 
 .data
@@ -13,6 +14,8 @@ txt_B:
     .ascii "Wprowadz b:\n"
 txt_C:
     .ascii "Podaj dzialanie:\n"
+io_buffer:
+	.ascii "                                                                "
 
 .text
 .global _start
@@ -31,19 +34,40 @@ txt_C:
 	int $kernel
 .endm
 
-.macro input length
-    ;Read and store the user input
-   mov eax, $stdin
-   mov ebx, 2
-   mov ecx, num  
-   mov edx, 5          ;5 bytes (numeric, 1 for sign) of that information
-   int $kernel
-.endm
-
-
 _start:
+	mov $0x10, %eax # read 16 bytes
+	call read_stdin
 
-    disp_str txt_A, $11
+	cmp $0, %eax
+	jz _exit
+
+    disp_str $txt_A, $11
+	disp_str $io_buffer, $64
 
 _exit:
     exit_prog $0
+
+#----------------------------------------
+#
+#	@func: read_stdin
+# 	@args: %eax - bytes to read
+# 	@retu: %eax - read bytes
+# 	@note: Does not save the eax!
+#
+.type read_stdin, @function
+read_stdin:
+	push %rbx
+	push %rcx
+	push %rdx
+	
+	mov %eax, %edx # how much to read
+	mov $read, %eax # sys func index
+	mov $stdin, %ebx # from stdin
+	mov $io_buffer, %ecx # into this buffer
+	int $kernel
+
+	pop %rdx
+	pop %rcx
+	pop %rbx
+	
+	ret
