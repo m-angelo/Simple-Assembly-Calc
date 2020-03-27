bb20d3f531ea4a27592de606970b4e30c347f0e1
m-angelo <mposluszny.foto@gmail.com>
2020-03-27 15:25:33 +0100

    Result is printed correctly

--- calc.s
+++ calc.s
@@ -10,12 +10,21 @@
 .data
 
 counter: .byte 0x00
+
 txt_A:
     .ascii "Wprowadz a:\n"
 txt_B:
     .ascii "Wprowadz b:\n"
 txt_C:
     .ascii "Podaj dzialanie:\n"
+text_eq:	
+	.string	" = "
+text_result:	
+	.string	"     \n"
+dataend:
+	.equ	text_len, dataend - text_eq
+
+
 io_buffer:
 	.ascii "                                                                "
 poll_fd:
@@ -42,6 +51,24 @@ poll_revents:
 	int $kernel
 .endm
 
+.macro addition num1, num2
+	mov \num1, %eax
+	mov \num2, %ebx
+	add  %ebx,%eax
+	mov %eax ,%ebx
+	call make_string
+	
+.endm
+
+.macro multiplication num1, num2
+	MOV \num1, %AL
+	MOV \num2, %DL
+	IMUL %DL
+	mov %eax ,%ebx
+	call make_string
+	
+.endm
+
 _start:
 	mov $stdin, %eax # poll stdin
 	call poll_fd_read
@@ -54,8 +81,60 @@ _start:
 
     disp_str $txt_A, $11
 	disp_str $io_buffer, $64
+
 _exit:
     exit_prog $0
+
+
+# rdi - starting index, ebx string
+.type make_string,@function
+make_string:
+	MOVL $0x20202020,text_result
+	MOV	%ebx,%eax		# convert index of vector element to string
+	MOV	$text_result,%rdi
+	CALL	num2dec
+	RET	
+
+#	Function:	num2dec
+#	Parameters:	%eax - value
+#			%rdi - address of last character
+.type num2dec,@function
+num2dec:
+
+	PUSH	%rbx		# save register on stack
+	PUSH	%rdx		# save register on stack
+	MOV	$10,%ebx	# divisor in EBX, dividend in EAX
+	MOV %rdi, %r13
+	mov $0, %r12
+nextdig:			
+	XOR	%edx,%edx	# EDX = 0
+	DIV	%ebx		# EDX:EAX div EBX
+	ADD	$'0',%dl	# convert remainder (in EDX) to character
+	push %rdx
+	CMP	$0,%eax		# quotient in EAX 
+
+	JZ	fill_string
+	INC %r12
+	DEC	%rdi		# RDI--
+	
+
+	JMP	nextdig	
+
+fill_string:
+	POP %rdx
+	MOV	%dl,(%r13)	# *(RDI) = character (decimal digit)
+	CMP	$0,%r12
+	JZ	empty
+	DEC %r12
+	INC	%r13
+	jmp fill_string
+
+
+empty:		
+	POP	%rdx		# restore register from stack
+	POP	%rbx		# restore register from stack
+	RET
+
 
 #----------------------------------------
 #
